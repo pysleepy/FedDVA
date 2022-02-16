e293d5d3ca9582f151ae0513af8a8fcdfa88be1d
pengyan <yanpeng9008@hotmail.com>
2022-02-16 15:14:22 +1100

    Init

--- visualize_tools.py
+++ visualize_tools.py
@@ -1,3 +1,7 @@
+import os.path
+
+import numpy as np
+
 import torch
 import matplotlib.pyplot as plt
 
@@ -26,63 +30,36 @@ def generate_coordinates(n_row, n_col, min_z, max_z, min_c, max_c):
     return z, c, z_labels, c_labels
 
 
-def parse_logs(c_id, path_to_log):
-    logs = []
+def parse_logs(client_root, c_id, log_name):
+    path_to_log = os.path.join(client_root, str(c_id), "logs", log_name)
+    epc_per_round = 5
+    loss_types = ["Decoder", "Decoder_z", "DKL z", "Decoder_c", "DKL c", "Constr c"]
 
-    log_name = 'celeba_no_resample_loss_d1'
+    loss = dict()
+    tmp_loss = dict()
 
-    with open(log_name) as f:
+    for tp in loss_types:
+        loss[tp] = []
+        tmp_loss[tp] = []
+
+    with open(path_to_log) as f:
         for line in f:
-            logs.append(line)
-
-    for c_id in range(5):
-        # c_id = 4
-
-        dec_c_loss = []
-        dec_z_loss = []
-
-        for r, l in enumerate(logs):
-            l = l.strip().split(" ")
-            print(l)
-            if 'Client:' in l and l[3] == str(c_id) + ',':
-                if r + 1 >= len(logs):
-                    break
-                ll = logs[r + 1].split(" ")
-                if 'Loss_c:' in ll:
-                    dec_c_loss.append(float(ll[-1]))
-                if 'Loss_z:' in ll:
-                    dec_z_loss.append(float(ll[-1]))
-
-        if len(dec_z_loss) == 0:
-            dec_z_loss = dec_c_loss
-
-        dc = []
-        dz = []
-        mc = 0.
-        mz = 0.
-        print(len(dec_z_loss))
-        print(len(dec_c_loss))
-        for r_id, (c, z) in enumerate(zip(dec_c_loss, dec_z_loss)):
-            mc += c
-            mz += z
-            if (r_id + 1) % 5 == 0:
-                dc.append(mc / 5.)
-                dz.append(mz / 5.)
-                mc = 0.
-                mz = 0.
-
-        dec_c_loss = dc
-        dec_z_loss = dz
-
-        print(len(dec_c_loss))
+            line = line.strip()
+            if line.startswith("Epoch"):
+                line = line.split("Epoch")[1].strip()
+                k, v = line.split("Loss")
+                k, v = k.strip(), float(v.strip().strip(":").strip())
+                tmp_loss[k].append(v)
+                if len(tmp_loss[k]) % epc_per_round == 0:
+                    loss[k].append(np.mean(tmp_loss[k]))
+                    tmp_loss[k] = []
 
         plt.figure()
         plt.title(log_name + " " + "Client: {:d}".format(c_id))
-        l1, = plt.plot(dec_c_loss, label='dec_c')
-        l2, = plt.plot(dec_z_loss, label='dec_z')
+        l1, = plt.plot(loss["Decoder"], label='loss_dec')
+        l2, = plt.plot(loss["Decoder_z"], label='loss_z')
+        l3, = plt.plot(loss["Decoder_c"], label='loss_c')
         # plt.legend(handles=[l1, l2], labels=['dec_c', 'dec_z'], loc='best')
 
         plt.legend()
         plt.show()
-
-
