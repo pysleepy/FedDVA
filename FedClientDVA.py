50e3ae73a03ace751076fb381622adf13e3ea7ea
pengyan <yanpeng9008@hotmail.com>
2022-02-26 23:24:41 +1100

    Init

--- FedClientDVA.py
+++ FedClientDVA.py
@@ -177,7 +177,7 @@ class FedClient:
                 # tmp
                 loss_dkl_c = loss_dkl(mu_c, log_var_c, mu_c_prior, log_var_c_prior)  # N(0, 1)
                 loss_dkl_c_local = loss_dkl(mu_c, log_var_c, mu_c_prior_local, log_var_c_prior)  # N(mu_c, 1)
-                loss_dkl_c_reverse = loss_dkl_c(mu_c_prior, log_var_c_prior, mu_c, log_var_c)
+                loss_dkl_c_reverse = loss_dkl(mu_c_prior, log_var_c_prior, mu_c, log_var_c)
 
                 loss_constr_c = loss_reg_c(mu_c, log_var_c)
                 loss_constr_c_2 = loss_reg_c_2(mu_c, log_var_c)
