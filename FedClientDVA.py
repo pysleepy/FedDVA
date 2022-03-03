f01e003fe002d37daeb88a7739d6348097e38a4d
pengyan <yanpeng9008@hotmail.com>
2022-03-03 20:30:20 +1100

    refactor

--- FedClientDVA.py
+++ FedClientDVA.py
@@ -189,8 +189,10 @@ class FedClient:
 
                 # 2022-02-24 loss = self.lbd_dec * loss_dec_c + self.lbd_c * loss_dkl_c \
                 # + self.lbd_cc * F.relu(self.xi + loss_constr_c - loss_dkl_c)
+                # 2022-03-03 loss = self.lbd_dec * loss_dec_c + self.lbd_c * loss_dkl_c_local \
+                # + self.lbd_cc * F.relu(self.xi + loss_constr_c - loss_dkl_c)
                 loss = self.lbd_dec * loss_dec_c + self.lbd_c * loss_dkl_c_local \
-                    + self.lbd_cc * F.relu(self.xi + loss_constr_c - loss_dkl_c)
+                    + self.lbd_cc * F.relu(self.xi + loss_dkl_c_local - loss_dkl_c)
 
                 loss = torch.mean(loss, dim=0)
                 loss.backward()
