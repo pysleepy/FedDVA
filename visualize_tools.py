d27e186d620ce86e62308cbfbfd8c9e4f157cadb
pengyan <yanpeng9008@hotmail.com>
2022-04-05 13:25:32 +1000

    refactor

--- visualize_tools.py
+++ visualize_tools.py
@@ -5,6 +5,47 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
+"""
+def saliency(img, model):
+    # we don't need gradients w.r.t. weights for a trained model
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # set model in eval mode
+    model.eval()
+    # transoform input PIL image to torch.Tensor and normalize
+    input = transform(img)
+    input.unsqueeze_(0)
+
+    # we want to calculate gradient of higest score w.r.t. input
+    # so set requires_grad to True for input
+    input.requires_grad = True
+    # forward pass to calculate predictions
+    preds = model(input)
+    score, indices = torch.max(preds, 1)
+    # backward pass to get gradients of score predicted class w.r.t. input image
+    score.backward()
+    # get max along channel axis
+    slc, _ = torch.max(torch.abs(input.grad[0]), dim=0)
+    # normalize to [0..1]
+    slc = (slc - slc.min()) / (slc.max() - slc.min())
+
+    # apply inverse transform on image
+    with torch.no_grad():
+        input_img = inv_normalize(input[0])
+    # plot image and its saleincy map
+    plt.figure(figsize=(10, 10))
+    plt.subplot(1, 2, 1)
+    plt.imshow(np.transpose(input_img.detach().numpy(), (1, 2, 0)))
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(1, 2, 2)
+    plt.imshow(slc.numpy(), cmap=plt.cm.hot)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+"""
+
 
 def generate_coordinates(n_row, n_col, min_z, max_z, min_c, max_c):
     # generate coordinates
@@ -72,6 +113,7 @@ def parse_multiple_logs(client_root, c_id, log_names, loss_type):
     tmp_loss = dict()
 
     for log_name in log_names:
+        print(log_name)
         path_to_log = os.path.join(client_root, str(c_id), "logs", log_name)
         loss[log_name] = []
         tmp_loss[log_name] = []
